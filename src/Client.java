1f80c301f0d32b3289849a61e7db3348840c1d51
SobirovI <88648901+SobirovI@users.noreply.github.com>
2021-12-05 17:00:06 +0500

    users scene and logout added

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,6 @@
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -6,7 +8,8 @@ import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import java.io.*;
 import java.net.*;
-
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.io.IOException;
 
 public class Client extends Application {
@@ -36,13 +39,26 @@ public class Client extends Application {
     HBox Log_3 = new HBox();
     VBox Log_Vertical = new VBox();
     
+    Scene ChatScene;
+    public static final ObservableList data = FXCollections.observableArrayList();
+    final ListView listViewMsg = new ListView(data);
+    TextArea Message;
+    Button Send, SeeUsers, Logout;
+    HBox Chat_1 = new HBox();
+    HBox Chat_2 = new HBox();
+    VBox Chat_Vertical = new VBox();
 
-    
+    Scene UsersScene;
+    public static final ObservableList Users = FXCollections.observableArrayList();
+    final ListView listViewUsers = new ListView(Users);
+    Button back_main;
+    Label title = new Label("All registered users");
+    VBox User_Vertical = new VBox();
     
 
     @Override
-    public void start(Stage start_stage) throws IOException {    
-        Socket socket = new Socket("localhost", 8000);
+    public void start(Stage start_stage) throws IOException, ClassNotFoundException {    
+        Socket socket = new Socket("192.168.100.40", 8000);
         DataOutputStream toServer = new DataOutputStream(socket.getOutputStream());
         DataInputStream fromServer = new DataInputStream(socket.getInputStream());
         stage = start_stage;
@@ -51,6 +67,8 @@ public class Client extends Application {
         LoginScene = createLoginScene(toServer,  fromServer);
         RegScene = createRegisterScene(toServer,  fromServer);
         
+        UsersScene = createUserScene(toServer, fromServer);
+
         stage.setScene(LoginScene);
         stage.show();
     }
@@ -75,7 +93,11 @@ public class Client extends Application {
 				to.writeUTF(RegPassword.getText());
 				to.flush();
                 System.out.println(RegPassword.getText() + "sent");
-
+                Boolean authed = from.readBoolean();
+                System.out.println(authed + " got");
+                if(authed) {
+                    switchScenes(ChatScene);
+                }
             } catch (IOException ex) 
             {
                 System.out.println(ex.toString() + '\n');
@@ -110,7 +132,6 @@ public class Client extends Application {
         submit = new Button("Submit");
         submit.setStyle("-fx-background-color: #00FFFF");
         submit.setOnAction(p->{
-           
             try {        
                 to.writeInt(1);        
 				to.writeUTF(LogLogin.getText());
@@ -121,15 +142,19 @@ public class Client extends Application {
                 System.out.println(LogPassword.getText() + " sent");
 				Boolean authed = from.readBoolean();
                 System.out.println(authed + " got");
+                if(authed) {
+                    ChatScene = createChatScene(to, from);
+                    switchScenes(ChatScene);
+                } else {
+                    switchScenes(RegScene);
+                }
                 
-            } catch (IOException ex) 
+            } catch (IOException | ClassNotFoundException ex) 
             {
                 System.out.println(ex.toString() + '\n');
             }
         });
 
-
-        
         register = new Button("Register");
         register.setStyle("-fx-background-color: #00FFFF");
         register.setOnAction(actionEvent -> {            
@@ -145,6 +170,100 @@ public class Client extends Application {
         return temp_scene;
     }
 
+    private Scene createChatScene(DataOutputStream to, DataInputStream from) throws IOException, ClassNotFoundException {
+        getMessages(to, from);
+        Message = new TextArea();
+        Message.setPrefHeight(40);
+        Message.setPrefWidth(150);
+        Send = new Button("Send");
+        Send.setPrefHeight(40);
+        Send.setPrefWidth(50);
+        Send.setOnAction(p-> {
+            try {            
+                to.writeInt(4); 
+                String text = LogLogin.getText() != "" ? LogLogin.getText() : RegLogin.getText();           
+				to.writeUTF(text);
+				to.flush();
+				to.writeUTF(Message.getText());
+				to.flush();
+                Message.clear();
+                data.clear();
+                getMessages(to, from);
+                switchScenes(ChatScene);
+            } catch (IOException | ClassNotFoundException ex) 
+            {
+                System.out.println(ex.toString() + '\n');
+            }
+        });
+
+        SeeUsers = new Button("Users");
+        SeeUsers.setPrefHeight(40);
+        SeeUsers.setPrefWidth(50);
+        SeeUsers.setOnAction(p-> {
+            switchScenes(UsersScene);
+        });
+
+        Logout = new Button("Logout");
+        Logout.setPrefHeight(40);
+        Logout.setPrefWidth(70);
+        Logout.setOnAction(p-> {
+            switchScenes(LoginScene);
+        });
+
+        Chat_1.getChildren().addAll(SeeUsers, Logout);
+        Chat_1.setSpacing(87);
+
+        Chat_2.getChildren().addAll(Message, Send);
+        Chat_2.setSpacing(20);
+        Chat_Vertical.getChildren().addAll(Chat_1, listViewMsg, Chat_2);
+        Chat_Vertical.setSpacing(20);
+        Chat_Vertical.setPadding(new Insets(20, 20, 20, 20));
+        Scene temp_scene = new Scene(Chat_Vertical, 250, 300);
+        return temp_scene;
+
+    }
+
+
+    private Scene createUserScene(DataOutputStream to, DataInputStream from) throws IOException, ClassNotFoundException {
+        to.writeInt(5);
+        ObjectInputStream ois = new ObjectInputStream(from);  
+        ArrayList users = (ArrayList) ois.readObject();
+        System.out.println(users);
+        listViewUsers.setPrefSize(190, 210);
+        listViewUsers.setEditable(true);            
+        for (int i = 0; i < users.size(); i++) {
+            String user = (String) users.get(i);
+            Users.add(user);
+        }          
+        listViewUsers.setItems(Users);
+        back_main = new Button("Back to main");
+        back_main.setOnAction(p-> {
+            switchScenes(ChatScene);
+        });
+        User_Vertical.getChildren().addAll(title, listViewUsers, back_main);
+        User_Vertical.setSpacing(10);
+        User_Vertical.setPadding(new Insets(15, 15, 15, 15));
+        Scene temp_scene = new Scene(User_Vertical, 200, 200);
+        return temp_scene; 
+    }
+
+    private void getMessages(DataOutputStream to, DataInputStream from) throws IOException, ClassNotFoundException {
+        to.writeInt(3);
+        ObjectInputStream ois = new ObjectInputStream(from);  
+        ArrayList messages = (ArrayList) ois.readObject();
+        System.out.println(messages);
+        listViewMsg.setPrefSize(190, 210);
+        listViewMsg.setEditable(true);
+        for (int i = 0; i < messages.size(); i++) {
+            HashMap one_m = (HashMap) messages.get(i);
+            System.out.println(one_m);
+            String sender = (String) one_m.get("sender");
+            String text = (String) one_m.get("text");
+            data.add(sender.toUpperCase() + ": " + text);
+        }          
+        listViewMsg.setItems(data);   
+    }
+    
     public void switchScenes(Scene scene) {
 		stage.setScene(scene);
 	}
--- src/Server.java
+++ src/Server.java
@@ -1,58 +1,88 @@
 import java.io.*;
 import java.sql.*;
+import java.util.*;
 import java.net.*;
 
-public class Server
-{
+public class Server{
   
-	public static void main(String[] args) 
+public static void main(String[] args) 
+{
+	new Server ();
+}
+public Server ()// constructor
+{ 
+	try 
 	{
-		new Server ();
-	}
-
-	public Server ()// constructor
-	{ 
-		try 
-		{
-			// Create a server socket
-			ServerSocket serverSocket = new ServerSocket(8000);
-			System.out.println("Server started at \n");
-			// Listen for a connection request
-			Socket socket = serverSocket.accept();
-			// Create data input and output streams
-			DataInputStream inputFromClient = new DataInputStream(socket.getInputStream());
-			DataOutputStream outputToClient = new DataOutputStream(socket.getOutputStream());
-			// Receive radius from the client
-			while(true) {
-				try {
-					Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/fast_food?autoReconnect=true&useSSL=false", "root", "mypassword");            
-            	    Statement stmt = conn.createStatement();
-					int type = inputFromClient.readInt();
-					switch(type) {
-						case 1: 
+		// Create a server socket
+		ServerSocket serverSocket = new ServerSocket(8000);
+		System.out.println("Server started at \n");
+		// Listen for a connection request
+		Socket socket = serverSocket.accept();
+		// Create data input and output streams
+		DataInputStream inputFromClient = new DataInputStream(socket.getInputStream());
+		DataOutputStream outputToClient = new DataOutputStream(socket.getOutputStream());
+		// Receive radius from the client
+		while(true) {
+			try {
+				Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/chat?autoReconnect=true&useSSL=false", "root", "mypassword");            
+        	    Statement stmt = conn.createStatement();
+				int type = inputFromClient.readInt();
+				switch(type) {
+					case 1: 
 						String strSelect = "select login, password from users";
-            	    	ResultSet rset = stmt.executeQuery(strSelect);
+        	    		ResultSet rset = stmt.executeQuery(strSelect);
 						boolean get_authed = false;
 						String login = inputFromClient.readUTF();
 						System.out.println("We have received login value "+ login);
 						String password = inputFromClient.readUTF();
 						System.out.println("We have received password value "+ password);
 						while(rset.next()) 
-            	    	{  
-            	    	    if(login.equals(rset.getString("login")) && password.equals(rset.getString("password"))) {
-            	    	        get_authed = true;
-            	    	    } 
-            	    	} 
+        	    		{  
+        	    		    if(login.equals(rset.getString("login")) && password.equals(rset.getString("password"))) {
+        	    		        get_authed = true;
+        	    		    } 
+        	    		} 
 						outputToClient.writeBoolean(get_authed); 
 						break;
-						case 2:
+					case 2:
 						String Reguser = inputFromClient.readUTF();
 						System.out.println("We received login value "+ Reguser);
 						String Regpassword = inputFromClient.readUTF();
 						System.out.println("We received password value "+ Regpassword);
 						String strInsert = "insert into users values (\"" + Reguser + "\", \"" + Regpassword + "\")"; 
-						int count= stmt.executeUpdate(strInsert);
-						System.out.println(count);
+						int count = stmt.executeUpdate(strInsert);
+						outputToClient.writeBoolean(count == 1); 
+						break;
+					case 3:
+						ArrayList list = new ArrayList();
+						String strSelectChat = "select sender, text from messages";
+        	    		ResultSet rsetChat = stmt.executeQuery(strSelectChat);
+						while(rsetChat.next()) 
+        	    		{  
+							HashMap mMap = new HashMap();
+							mMap.put("sender", rsetChat.getString("sender"));
+							mMap.put("text",rsetChat.getString("text"));
+							list.add(mMap);
+        	    		} 
+						ObjectOutputStream oos = new ObjectOutputStream(outputToClient);  
+						oos.writeObject(list);
+						break;
+					case 4:
+						String Sender = inputFromClient.readUTF();
+						String Text = inputFromClient.readUTF();
+						System.out.println("We received " + Text + " from " + Sender);
+						String MSGInsert = "insert into messages values (\"" + Sender + "\", \"" + Text + "\")"; 
+						stmt.executeUpdate(MSGInsert);
+						break;
+					case 5:
+						ArrayList users = new ArrayList();
+						String strSelectUsers = "select login, password from users";
+						ResultSet rsetUsers = stmt.executeQuery(strSelectUsers);
+						while(rsetUsers.next()) {  
+							users.add(rsetUsers.getString("login"));
+						} 
+						ObjectOutputStream oostr = new ObjectOutputStream(outputToClient);  
+						oostr.writeObject(users);
 						break;
 					}
 				} catch (SQLException e) {
